1918e9230faca6127ecffa622ae79d13fe46d897
William O Soares <williamoliveirafsa@outlook.com>
2023-05-22 21:46:06 -0300

    Uma tentativa de fazer o front

--- app/page.js
+++ app/page.js
@@ -16,28 +16,14 @@ function SearchBar({filterText, onFilterTextChange}) {
 }
 
 function MessageRow({ message }) {
+  //Mudei a forma que pega evento, se vai funcionar? Não sei
   return (
-    <tr onclick="getCity(message[1])"> 
+    <tr onclick={(e) => getCity(e.td[1].value)}> 
       <td>{message[0]}</td>
       <td>{message[1]}</td>
       <td>{message[2]}</td>
     </tr>
   );
-}
-
-function getCity(code){
-  var op = 1;
-  fetch("https://script.google.com/macros/s/AKfycbybzO8Wk0j8lU8jExj9mbdv7EaEu38lbG95uDg5R3lxAejbx9Jvvv5nS9YMp1_NsoaYCA/exec?op="+op+"&code="+code)
-    .then(response => response.json())
-    .then(data => {
-      setBlogMessages(data);
-    });
-    
-    return (
-      <main className={styles.main}>
-        <FilterableMessageTable messages={blogMessages} />
-      </main>
-    );
 }
 
 function FilterableMessageTable({ messages }) {
@@ -92,4 +78,58 @@ export default function Home() {
       </main>
     );
 
+}
+// Mudei daqui pra baixo, seguindo a lógica, mas sem teste
+function wheREterDados({ city }) {
+  const [filterText, setFilterText] = useState('');
+ 
+
+  return (
+    <div>
+      <SearchBar filterText={filterText} onFilterTextChange={setFilterText} />
+      <table>
+        <thead>
+          <tr>
+            <th>Cidade</th>
+            <th>Código</th>
+            <th>Estado</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>{city.temp_max}</th>
+            <th>{city.temp_min}</th>
+            <th>{city.umidade_max}</th>
+            <th>{city.umidade_min}</th>
+            <th>{city.estacao}</th>
+            <th>{city.resumo}</th>
+          </tr>  
+          <tr>
+            <th>{city.nascer}</th>
+            <th>{city.ocaso}</th>
+            <th>{city.dir_vento}</th>
+            <th>{city.int_vento}</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+
+function getCity(code){
+  const [cityInfo, setCityInfo] = useState([]);
+  var op = 1;
+  fetch("https://script.google.com/macros/s/AKfycbybzO8Wk0j8lU8jExj9mbdv7EaEu38lbG95uDg5R3lxAejbx9Jvvv5nS9YMp1_NsoaYCA/exec?op="+op+"&code="+code)
+    .then(response => response.json())
+    .then(data => {
+      setCityInfo(data);
+    });
+    
+    return (
+      <main className={styles.main}>
+        <FilterableMessageTable messages={blogMessages} />
+        <wheREterDados city={cityInfo}/>
+      </main>
+    );
 }
